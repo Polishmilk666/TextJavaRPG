8fac9296db8b9af38e971b8cad2d32f42525fa1a
Polishmilk666 <polishmilk@o2.pl>
2025-09-08 15:51:52 +0200

    invenotory and checkifplayerexists

--- src/main/java/org/example/model/Item.java
+++ src/main/java/org/example/model/Item.java
@@ -41,6 +41,6 @@ public class Item {
 
     @Override
     public String toString() {
-        return itemName + "(" + itemType+ " " + itemAttack + " " + itemDefence +")";
+        return "("+ ")"+ itemId + itemName + "(" + itemType+ " " + itemAttack + " " + itemDefence +")";
     }
 }
